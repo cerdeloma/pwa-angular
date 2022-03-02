d8893246918eccc445aaf0860e4b14607f7cff01
Marcelo Cerdelo <marcelocerdelo@gmail.com>
2022-03-02 02:16:32 -0300

    adjusting

--- src/app/components/listar/listar.component.ts
+++ src/app/components/listar/listar.component.ts
@@ -11,23 +11,33 @@ import { AppServicesService } from 'src/app/services/app-services.service';
 export class ListarComponent implements OnInit {
 
   dados: any;
+  key: any;
 
   constructor(private appService: AppServicesService) { }
 
   ngOnInit(): void {
     this.appService.getData().pipe(
-      tap(y => y),
-      map(x => (Object as any).values(x))
+      tap(x => {
+        this.key = Object.keys(x)
+      })
     ).subscribe(
       (res: any) => {
-        // console.log(res);
-        this.dados = res;
+        this.dados = Object.values(res);
+        console.log(this.key);
       }
     )
+    // .pipe(
+    //   map(x => (Object as any).values(x)),
+    // ).subscribe(
+    //   (res: any) => {
+    //     // console.log(res);
+    //     this.dados = res;
+    //   }
+    // )
   }
 
-  onDelete(i?: any) {
-    this.appService.deleteData(i).subscribe(
+  onDelete(key?: any) {
+    this.appService.deleteData(key).subscribe(
       (res: any) => {
         console.log(res);
       }
