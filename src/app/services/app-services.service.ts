87d5e305fd0d0582a65824b524d3e5fc14b59045
Marcelo Cerdelo <marcelocerdelo@gmail.com>
2022-03-03 15:11:22 -0300

    creating abstract service

--- src/app/services/app-services.service.ts
+++ src/app/services/app-services.service.ts
@@ -1,55 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Injectable, Injector } from '@angular/core';
 import Dexie from 'dexie';
 import { map, tap } from 'rxjs/operators';
 import { Contato } from '../models/contato';
-import { OnlineOfflineService } from './online-offline.service';
+import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppServicesService {
+export class AppServicesService extends BaseService<Contato> {
 
   private API = 'https://pwa-angular-e6732-default-rtdb.firebaseio.com/';
-  private db!: Dexie;
-  private table!: Dexie.Table<any, any>;
 
   constructor(
-    private http: HttpClient,
-    private onlineOfflineService: OnlineOfflineService,
-    private angularFireDb: AngularFireDatabase
-    ) {
-      this.ouvirStatusConexao();
-      this.iniciarIndexedDb();
-    }
-
-  insert(contato: Contato) {
-    this.angularFireDb.list('contato').push(contato).then(
-      (result: any) => {
-        console.log(result);
-      }
-    );
+    protected injector: Injector
+  ) {
+    super(injector, 'contato', 'contato', 'https://pwa-angular-e6732-default-rtdb.firebaseio.com/');
   }
-
-  update(contato: Contato, key: string) {
-    this.angularFireDb.list('contato').update(key, contato);
-  }
-
-  getAll() {
-    return this.angularFireDb.list('contato')
-    .snapshotChanges()
-    .pipe(
-      map(changes => {
-        return changes.map(data => ({key: data.payload.key, ...data.payload.val() as any}))
-      })
-    )
-  }
-
-  delete(key: string) {
-    this.angularFireDb.object(`contato/${key}`).remove();
-  }
-  
 
   postForm(body: any) {
     return this.http.post(`${this.API}contato.json`, body);
@@ -71,43 +38,5 @@ export class AppServicesService {
     return this.http.patch(`${this.API}contato/${id}.json`, body);
   }
 
-  private iniciarIndexedDb(){
-    this.db = new Dexie('db-contato');
-    this.db.version(1).stores({
-      contato: 'id'
-    });
-    this.table = this.db.table('contato');
-  }
-
-  public async salvarIndexedDb(body: any) {
-    try {
-      await this.table.add(body);
-      // const todosContatos = await this.table.toArray();
-      // console.log('seguro foi salvo com indexedDb', todosContatos);
-    } catch (error) {
-      console.log('erro ao cadastrar contato no indexedDb', error);
-    }
-  }
-
-  private async enviarIndexedDbParaApi() {
-    const todosContatos = await this.table.toArray();
-    for (const contato of todosContatos) {
-      this.postForm(contato).subscribe();
-      await this.table.delete(contato?.id);
-      // console.log(`Seguro com o id ${contato?.id} foi excluido com sucesso`)
-    }
-  }
-
-  private ouvirStatusConexao() {
-    this.onlineOfflineService.statusConexao.subscribe(
-      (online: any) => {
-        if (online) {
-          this.enviarIndexedDbParaApi();
-        } else {
-          console.log('estou offline');
-        }
-      }
-    );
-  }
 
 }
---
+++ src/app/services/base.service.ts
@@ -0,0 +1,107 @@
+import { OnlineOfflineService } from 'src/app/services/online-offline.service';
+import { HttpClient } from '@angular/common/http';
+import { Dexie } from 'dexie';
+import { Inject, Injectable, Injector } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class BaseService<T extends {id: any}> {
+
+  private db!: Dexie;
+  private table!: Dexie.Table<T, any>;
+
+  protected http!: HttpClient;
+  protected onlineOfflineService!: OnlineOfflineService;
+  protected angularFireDb!: AngularFireDatabase;
+
+
+  constructor(
+    // injeta os serviços no construtor via injector
+    protected injector: Injector,
+    @Inject(String) protected nomeTabela: string,
+    @Inject(String) protected apiDb: string,
+    @Inject(String) protected urlApi: string,
+  ) {
+    this.http = this.injector.get(HttpClient);
+    this.onlineOfflineService = this.injector.get(OnlineOfflineService);
+    this.angularFireDb = this.injector.get(AngularFireDatabase);
+    this.ouvirStatusConexao();
+    this.iniciarIndexedDb();
+  }
+
+  iniciarIndexedDb() {
+    this.db = new Dexie('db-contato');
+    this.db.version(1).stores({
+      [this.nomeTabela]: 'id'
+    });
+    this.table = this.db.table(this.nomeTabela);
+  }
+
+  insert(tabela: any) {
+    this.angularFireDb.list('tabela').push(tabela).then(
+      (result: any) => {
+        console.log(result);
+      }
+    );
+  }
+
+  update(tabela: any, key: string) {
+    this.angularFireDb.list(this.apiDb).update(key, tabela);
+  }
+
+  getAll() {
+    return this.angularFireDb.list(this.apiDb)
+    .snapshotChanges()
+    .pipe(
+      map((changes: any) => {
+        return changes.map((data: any) => ({key: data.payload.key, ...data.payload.val() as any}))
+      })
+    )
+  }
+
+  delete(key: string) {
+    this.angularFireDb.object(`tabela/${key}`).remove();
+  }
+
+  public async salvarIndexedDb(tabela: any) {
+    try {
+      await this.table.add(tabela);
+      // const todosContatos = await this.table.toArray();
+      // console.log('seguro foi salvo com indexedDb', todosContatos);
+    } catch (error) {
+      console.log('erro ao cadastrar contato no indexedDb', error);
+    }
+  }
+
+  private async enviarIndexedDbParaApi() {
+    const todosDados = await this.table.toArray();
+    for (const dado of todosDados) {
+      this.postForm(dado).subscribe();
+      await this.table.delete(dado?.id);
+      // console.log(`Seguro com o id ${contato?.id} foi excluido com sucesso`)
+    }
+  }
+
+  postForm(tabela: any) {
+    return this.http.post(`${this.urlApi}`, tabela);
+  }
+
+  private ouvirStatusConexao() {
+    this.onlineOfflineService.statusConexao.subscribe(
+      (online: any) => {
+        if (online) {
+          this.enviarIndexedDbParaApi();
+        } else {
+          console.log('estou offline');
+        }
+      }
+    );
+  }
+
+
+
+
+}
