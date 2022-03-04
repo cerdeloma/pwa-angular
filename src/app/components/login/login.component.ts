6adbc3d2148d140e705197454724ccf0cf6ee41b
Marcelo Cerdelo <marcelocerdelo@gmail.com>
2022-03-04 17:55:50 -0300

    adding login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -44,9 +44,9 @@ export class LoginComponent implements OnInit {
   entrarComGoogle() {
     const {email, senha} = this.usuario;
     this.authService.loginWithGoogle(email, senha).then((res: any) => {
-      console.log('email cadastrado', res);
       this.usuario.email = '';
       this.usuario.senha = '';
+      this.router.navigate(['redirect']);
     })
   }
 
