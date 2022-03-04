12caaf018ae7ca652a41bbbae19caf8cd299c848
Marcelo Cerdelo <marcelocerdelo@gmail.com>
2022-03-04 11:42:31 -0300

    initializing login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
+import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { ListarComponent } from './components/listar/listar.component';
@@ -8,8 +9,7 @@ const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomeComponent},
   {path: 'edit/:id', component: CadastroComponent},
-  // {path: 'cadastro', component: CadastroComponent},
-  // {path: 'listar', component: ListarComponent},
+  {path: 'login', component: LoginComponent},
   {path: '**', redirectTo: 'home'},
 ];
 
